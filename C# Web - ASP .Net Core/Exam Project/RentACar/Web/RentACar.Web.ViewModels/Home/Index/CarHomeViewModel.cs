b6e35731bb1b576ac49b6b0fe47808c128a10cee
VladAngelov <v.angelov1919@gmail.com>
2019-08-30 04:57:04 +0300

    Added pictures, to fix home page

---
+++ C# Web - ASP .Net Core/Exam Project/RentACar/Web/RentACar.Web.ViewModels/Home/Index/CarHomeViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace RentACar.Web.ViewModels.Home.Index
+{
+    using RentACar.Web.BindingModels;
+    using System;
+
+    public class CarHomeViewModel
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime ManufacturedOn { get; set; }
+
+        public decimal PricePerDay { get; set; }
+
+        public string Picture { get; set; }
+
+        public CarGroupBindingModel CarGroup { get; set; }
+
+        public CarEquipmentBindingModel AirConditioner { get; set; }
+
+        public CarEquipmentBindingModel AutomaticGearbox { get; set; }
+
+        public CarEquipmentBindingModel Disel { get; set; }
+
+        public bool IsBooked { get; set; }
+    }
+}
