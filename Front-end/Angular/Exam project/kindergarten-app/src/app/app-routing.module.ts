372c117c200fbe3f22d58db5f70776dc4df25daa
VladAngelov <v.angelov1919@gmail.com>
2020-12-04 19:11:45 +0200

    Added empty core and share.

--- Front-end/Angular/Exam project/kindergarten-app/src/app/app-routing.module.ts
+++ Front-end/Angular/Exam project/kindergarten-app/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+// export const AppRoutingModule = RouterModule.forRoot(routes);
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
